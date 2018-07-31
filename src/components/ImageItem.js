d9030e333c64cb92ad049ba8482448d8b365fec4
liy <liy8272@gmail.com>
2018-07-31 23:01:49 +0100

    Show description on mobile

--- src/components/ImageItem.js
+++ src/components/ImageItem.js
@@ -2,12 +2,21 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {addLike, removeLike} from '../actions';
 import './ImageItem.scss';
-import LikeButton from './LikeButton'
+import LikeButton from './LikeButton';
+import {isMobile} from '../utils';
 
 let ImageItem = ({tabIndex, store, isLiked, link, description, thumbnail}) => {
+
+  if(isMobile) {
+    // since no rollover on mobile, show description by default when on mobile devices
+    var style = {
+      opacity: 0.8
+    }
+  }
+
   return <div className='image-item'>
       <img tabIndex={tabIndex} alt={description} className='thumbnail-image' src={thumbnail}/>
-      <div className='description'>{/* add and remove from favourites */}
+      <div className='description' style={style}>{/* add and remove from favourites */}
         <LikeButton isLiked={isLiked} onClick={(e) => {
           if(isLiked) {
             store.dispatch(removeLike(link));
